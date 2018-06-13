438d9342347a92f6bb25cec134548696a7364a3f
lazylife7157 <lazylife7157@gmail.com>
2018-06-13 22:52:15 +0900

    Remove head link from config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,5 @@
 module.exports = {
   title: 'lazylife7157\'s blog',
-  head: [
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.10.0-alpha/dist/katex.min.css' }]
-  ],
   markdown: {
     config: md => {
       md.use(require("markdown-it-katex"));
