af52c57d79f18c671a621f7911508bcdf133428a
lazylife7157 <lazylife7157@gmail.com>
2018-06-17 23:44:34 +0900

    Replace markdown-it-katex with forked version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,11 @@
 module.exports = {
   title: 'lazylife7157\'s blog',
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.9.0/dist/katex.min.css' }]
+  ],
   markdown: {
     config: md => {
-      md.use(require("markdown-it-katex"));
+      md.use(require("@iktakahiro/markdown-it-katex"));
     }
   },
   module: {
